c6a1ecec91ed99b5d8e1626603ad48f2f3894d2b
Evan <evanjohnmoss@gmail.com>
2019-11-16 18:32:25 +0000

    coords fixed

--- image2.py
+++ image2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 #Z-X plane
-=======
-
->>>>>>> 71113963edabb64714ef0a0edfbd4393de47e8c9
 import roslib
 import sys
 import rospy
@@ -17,7 +13,6 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
 
-<<<<<<< HEAD
     # Defines publisher and subscriber
     def __init__(self):
         # initialize the node named image_processing
@@ -182,52 +177,7 @@ def main(args):
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
-=======
-  # Defines publisher and subscriber
-  def __init__(self):
-    # initialize the node named image_processing
-    rospy.init_node('image_processing', anonymous=True)
-    # initialize a publisher to send images from camera2 to a topic named image_topic2
-    self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
-    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
-    self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
-    # initialize the bridge between openCV and ROS
-    self.bridge = CvBridge()
-
-
-  # Recieve data, process it, and publish
-  def callback2(self,data):
-    # Recieve the image
-    try:
-      self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
-    # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
-    im2=cv2.imshow('window2', self.cv_image2)
-    cv2.waitKey(1)
-
-    # Publish the results
-    try: 
-      self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
-
-# call the class
-def main(args):
-  ic = image_converter()
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
->>>>>>> 71113963edabb64714ef0a0edfbd4393de47e8c9
 
 # run the code if the node is called
 if __name__ == '__main__':
     main(sys.argv)
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 71113963edabb64714ef0a0edfbd4393de47e8c9
